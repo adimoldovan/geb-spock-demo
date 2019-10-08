f4bae4dfc4592ca4733ae62f061e5c2e007a3361
Adi M <adim.moldovan@gmail.com>
2019-10-08 13:02:32 +0300

    Jenkins file

--- src/test/resources/Jenkinsfile.groovy
+++ src/test/resources/Jenkinsfile.groovy
@@ -1,41 +1,30 @@
 pipeline {
-    agent { label 'host' }
+    agent {label 'host'}
     stages {
-        stage('Set up') {
-            parallel {
-                stage('Checkout Code') {
-                    steps {
-                        git 'https://github.com/adimoldovan/geb-spock-demo.git'
-                    }
-                }
-                stage('Pull images') {
-                    steps {
-                        sh 'docker pull selenium/hub'
-                        sh 'docker pull selenium/node-chrome-debug'
-                    }
-                }
+        stage('Pull code and images') {
+            steps {
+                git 'https://github.com/adimoldovan/geb-spock-demo.git'
+                sh 'docker pull selenium/hub'
+                sh 'docker pull selenium/node-chrome-debug'
             }
-        } stage('Start Grid') {
+        }
+        stage('Start the grid') {
             steps {
                 sh 'docker network create grid'
                 sh 'docker run --rm -d -p 4444:4444 --net grid --name selenium-hub selenium/hub'
                 sh 'docker run --rm -d -p 5900:5900 --net grid -e HUB_HOST=selenium-hub -v /dev/shm:/dev/shm --name=selenium-node-chrome selenium/node-chrome-debug'
             }
         }
-
-        stage('Run Tests') {
+        stage('Run tests') {
             steps {
-                sh 'mvn clean test -Dsleep=5000 -Dgeb.env=grid'
+                sh 'mvn clean test -Dsleep=0 -Dgeb.env=grid'
             }
         }
-
-
     }
-    post('Clean up') {
+    post {
         always {
             sh 'docker container stop $(docker container ls -q --filter name=selenium*)'
             sh 'docker network rm grid'
         }
     }
-
 }
