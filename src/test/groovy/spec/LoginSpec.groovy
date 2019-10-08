3a834e5a31cdecd7bba7c3a77e0a7f895274df35
Adi M <adim.moldovan@gmail.com>
2019-10-08 12:12:50 +0300

    Added sleep option to slow tests down on need

--- src/test/groovy/spec/LoginSpec.groovy
+++ src/test/groovy/spec/LoginSpec.groovy
@@ -7,41 +7,52 @@ class LoginSpec extends BaseSpec {
 
     def setup() {
         to AuthPage
+        String time = System.getProperty('sleep')
+
+        if (time != null) {
+            try {
+                sleep(time.toInteger())
+                println("Sleeping for $time ms")
+            }
+            catch (Exception e) {
+                e.printStackTrace()
+            }
+        }
     }
 
     def "Successful login with user #username"(String username, String password) {
         when: "attempting login with valid credentials"
-            login(username, password)
+        login(username, password)
 
         then: "welcome page is displayed"
-            at WelcomePage
+        at WelcomePage
 
         and: "user is logged in"
-            isLoggedIn(username)
+        isLoggedIn(username)
 
         where:
-            username | password
-            'camel'  | 'camelpassword'
+        username | password
+        'camel'  | 'camelpassword'
     }
 
     def "Unsuccessful login: #description"(String description, String username, String password, String expectedUserError, String expectedPasswordError, String expectedGeneralError) {
         when: "attempting login with invalid credentials"
-            login(username, password)
+        login(username, password)
 
         then: "welcome page is displayed"
-            at AuthPage
+        at AuthPage
 
         and: "expected errors are displayed"
-            usernameErrorText == expectedUserError
-            passwordErrorText == expectedPasswordError
-            generalErrorText == expectedGeneralError
+        usernameErrorText == expectedUserError
+        passwordErrorText == expectedPasswordError
+        generalErrorText == expectedGeneralError
 
         where:
-            description             | username   | password   | expectedUserError            | expectedPasswordError        | expectedGeneralError
-            'missing user and pass' | ''         | ''         | 'Please enter your username' | 'Please enter your password' | ''
-            'missing password'      | 'camel'    | ''         | ''                           | 'Please enter your password' | ''
-            'missing username'      | ''         | 'somepass' | 'Please enter your username' | ''                           | ''
-            'wrong user and pass'   | 'someuser' | 'somepass' | ''                           | ''                           | 'Invalid username or password!'
-            'wrong password'        | 'camel'    | 'somepass' | ''                           | ''                           | 'Invalid username or password!'
+        description             | username   | password   | expectedUserError            | expectedPasswordError        | expectedGeneralError
+        'missing user and pass' | ''         | ''         | 'Please enter your username' | 'Please enter your password' | ''
+        'missing password'      | 'camel'    | ''         | ''                           | 'Please enter your password' | ''
+        'missing username'      | ''         | 'somepass' | 'Please enter your username' | ''                           | ''
+        'wrong user and pass'   | 'someuser' | 'somepass' | ''                           | ''                           | 'Invalid username or password!'
+        'wrong password'        | 'camel'    | 'somepass' | ''                           | ''                           | 'Invalid username or password!'
     }
 }
